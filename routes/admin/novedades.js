91eaa6144bee9876d1420d563f4c3fde53c064cf
Alejandro Rodón <amrodon@gmail.com>
2022-04-20 11:43:32 -0300

    cambios

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
   res.render('admin/novedades', {
-    layout: 'admin/layout'
+    layout: 'admin/layout',
+    usuario:req.session.nombre
   });
 });
 module.exports=router;
