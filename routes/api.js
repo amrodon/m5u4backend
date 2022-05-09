8715be94ce8cfbe4a29eee33f67ceed841f8f2eb
Alejandro Rodón <amrodon@gmail.com>
2022-05-08 21:20:03 -0300

    cambios

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var router=express.Router();
 var novedadesModel=require('./../models/novedadesModel');
-var nodemailer=require('nodemailer')
+var nodemailer=require('nodemailer');
 
 router.get('/novedades',async function(req,res,next){
     let novedades=await novedadesModel.getNovedades();
